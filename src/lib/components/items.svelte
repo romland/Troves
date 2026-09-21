71122b78c05205fd187f97573f084e0f9f62cabc
Joakim Romland <romland@gmail.com>
2026-09-21 15:03:55 +0200

    Indent

--- src/lib/components/items.svelte
+++ src/lib/components/items.svelte
@@ -386,11 +386,11 @@
                         </td>
 
                         {#if !brief && $page.data.role !== 'VIEWER'}
-                        <td class="whitespace-nowrap relative z-20">
-                            <a href="/{item.id}/edit" title="Edit Item" class="text-gray-500 hover:text-primary">
-                                <i class="bi bi-pencil-square"></i>
-                            </a>
-                        </td>
+                            <td class="whitespace-nowrap relative z-20">
+                                <a href="/{item.id}/edit" title="Edit Item" class="text-gray-500 hover:text-primary">
+                                    <i class="bi bi-pencil-square"></i>
+                                </a>
+                            </td>
                         {/if}                        
                     </tr>
                 {/each}
